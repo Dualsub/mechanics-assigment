0ef06d563aec7b3f3501f2732d1df342637a0ed4
Dualsub <rizedualsub@gmail.com>
2021-02-24 15:22:05 +0100

    Added submission pdf

--- equation_calc.py
+++ equation_calc.py
@@ -7,6 +7,9 @@ def printLatex(expr):
 
 # Symbolisk beräkning av differential ekvationen.
 def calc_diff():
+    
+    # A1
+    
     # Definerar variabler och funktioner
     t, omega, m, g = symbols("t, ω, m, g")
     xi, f = symbols("ξ, f", cls=Function)
@@ -19,16 +22,6 @@ def calc_diff():
     z = f_xi
 
     r = np.array([x,y,z])
-
-    # Tangentvektorn fås genom att derivera r med avseende på ξ(t).
-    tangent = np.array([
-        x.diff(xi_t), 
-        y.diff(xi_t), 
-        z.diff(xi_t)
-        ])
-
-    # Enhetsvektorn längsmed 
-    e_r = tangent / sqrt(np.sum(tangent**2))
 
     # Hastighet utvinns genom derivering av lägevektorn. 
     v = np.array([
@@ -44,13 +37,25 @@ def calc_diff():
         diff(z, t, t).simplify()
     ])
 
-    mae_r = np.dot(m*a, e_r)
+    # Tangentvektorn fås genom att derivera r med avseende på ξ(t).
+    T = np.array([
+        x.diff(xi_t), 
+        y.diff(xi_t), 
+        z.diff(xi_t)
+    ])
+
+    # A2
+
+    # Enhetsvektorn längsmed 
+    e_s = T / sqrt(np.sum(T**2))
+
+    VL = np.dot(m*a, e_s)
 
     F = np.array([0,0,-m*g])
 
-    Fe_r = np.dot(F, e_r).simplify()
+    HL = np.dot(F, e_s).simplify()
 
-    diffEq = Eq(Fe_r, mae_r)
+    diffEq = Eq(VL, HL)
 
     # Returnerar symboler så att användaren av funktionen kan utföra substitutioner.
     return diffEq.simplify(), xi_t, f_xi, t, omega, m, g
